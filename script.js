73216de7eb1de405ed6e258597fce16c1a86f4a9
wesley <wesley>
2022-11-08 16:50:22 -0300

    Starting with vars

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//declarando variaveis e atribuindo valores.
+var nome = 'wesley';
+var idade = 19;
+var possuifaculdade = false;
+
+console.log(nome, idade, possuifaculdade);
+
+//declarando a variavel, e depois atribuindo valor a ela.
+var comidaFav;
+
+comidaFav = 'burger';
+
+console.log(comidaFav);
+
+//declarando variaveis diferentes sem usar o var a todo momento
+var instrumento1,
+    instrumento2,
+    instrumento3,
+    instrumento4,
+    instrumento5;
+
+//atribuindo valores as variaveis
+    instrumento1 = 'guitarra';
+    instrumento2 = 'violão';
+    instrumento3 = 'baixo';
+    instrumento4 = 'teclado';
+    instrumento5 = 'ukulele'; 
+
+    console.log(instrumento1, instrumento2, instrumento3, instrumento4, instrumento5);
